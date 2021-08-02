a8a2608834f53391d1ef0ca8da53f513830c8064
Russell Gordon <rgordon@lcs.on.ca>
2021-08-02 09:44:31 -0400

    Initial Commit

---
+++ Sandbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sandbox
+//
+//  Created by Russell Gordon on 2021-08-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Russell Gordon on 2021-08-02.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
